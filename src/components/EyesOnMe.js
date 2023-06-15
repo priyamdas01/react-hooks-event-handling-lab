7c0f5de8b2e0adb4b4c6465169a1390c94a0ded0
Priyam Das <domidas01@gmail.com>
2023-06-14 22:44:57 -0400

    complete lab

--- src/components/EyesOnMe.js
+++ src/components/EyesOnMe.js
@@ -1,1 +1,16 @@
 // Code EyesOnMe Component Here
+
+
+
+// Code EyesOnMe Component Here
+function onBlurHandler(event){
+    console.log("Hey! Eyes on me!")
+}
+
+function EyesOnMe(){
+    return (
+        <button onFocus={event=>console.log("Good!")} onBlur={onBlurHandler}>Eyes on me</button>
+    );
+}
+
+export default EyesOnMe;
--- src/components/Keypad.js
+++ src/components/Keypad.js
@@ -1,9 +1,11 @@
 // Code Keypad Component Here
-
 function Keypad (){
     return (
-        <div></div>
+        <div>
+            <input type="password" onChange={event=>console.log("Entering password...")}></input>
+        </div>
     )
 }
 
 export default Keypad;
+
